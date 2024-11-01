0ee33a9b278af23d39bc905b89277a285fdf9ea4
jiangshanruhua24 <774624654@qq.com>
2024-11-01 13:27:14 +0800

    1

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,11 @@
 <template>
-  <router-view/>
-  <SET />
+  <div id="app">
+    <router-view />
+  </div>
 </template>
-<script lang="ts">
-import { defineComponent, nextTick, onMounted } from 'vue'
-import SET from "@/components/set/set.vue";
-export default defineComponent({
-  setup(props) {
-    let theme=window.localStorage.getItem("theme");
-    theme&&window.document.documentElement.setAttribute("data-ming",theme);
-  },
-  components:{SET}
-})
-</script>
 
-<style lang="scss">
-  *{
-    margin: 0;
-  }
-  
-</style>
+<script>
+export default  {
+  name:  'App'
+}
+</script>
--- vue.config.js
+++ vue.config.js
@@ -1,41 +1,20 @@
-const path = require("path");
-function resolve(dir) {
-    return path.join(__dirname, dir);
+const { defineConfig } = require('@vue/cli-service')
+const webpack = require("webpack");
+module.exports = defineConfig({
+  devServer:{
+    // 端口号的配置
+		port:1024  
+	},
+  transpileDependencies: true,
+  lintOnSave: false,
+  configureWebpack: {
+    plugins: [
+        new webpack.ProvidePlugin({
+            $:"jquery",
+            jQuery:"jquery",
+            "windows.jQuery":"jquery"
+        })
+    ]
 }
+})
 
-module.exports = {
-    chainWebpack: config => {
-        // 清除svg默认配置对./src/icons文件夹的处理
-        config.module
-            .rule("svg")
-            .exclude.add(resolve("src/assets/icons")) // icons存放地（svg放的地方）
-            .end()
-
-        // 添加新的rule处理./src/icons文件夹的svg文件
-        config.module
-            .rule("svg-sprite")
-            .test(/\.svg$/)
-            .include
-            .add(resolve("src/assets/icons"))
-            .end()
-            .use("svg-sprite-loader")
-            .loader("svg-sprite-loader")
-            .options({
-                symbolId: "icon-[name]",
-                include: ["src/assets/icons"]
-            })
-            .end()
-            .before("svg-sprite-loader")
-            .use("svgo-loader")
-            .loader("svgo-loader")
-            .options({
-                plugins: [
-                    { removeAttrs: { attrs: "path:fill" } }
-                ]
-            })
-            .end()
-    },
-    devServer:{
-        open:true
-    }
-}
